f2f49c8ccaa68a22f016a43e792c71611e4e3d22
GlenSu <glensu4@gmail.com>
2015-09-17 13:41:21 -0400

    Adding the programs HelloWorld, Greetings, and Greetings2

---
+++ Hello/src/glen/Greetings.java
@@ -0,0 +1,13 @@
+package glen;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = args[0];
+		String lastName = args[1];
+		System.out.println("Hello " + firstName + " " + lastName);
+		System.out.println("Congratulations on your second program!");
+	}
+
+}
